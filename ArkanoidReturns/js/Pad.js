bc512bd4b2d983976f3ac2f23b2408d0c1c6104f
Iris LC <iris.lazaro@enti.cat>
2023-11-10 12:34:45 +0100

    Apply bounce on overlap according to pad zone (untested)

--- ArkanoidReturns/js/Pad.js
+++ ArkanoidReturns/js/Pad.js
@@ -1,13 +1,13 @@
 class Pad extends Phaser.GameObjects.Sprite
 {
-    constructor(scene, positionX, positionY, spriteTag){
+    constructor(_scene, _positionX, _positionY, _spriteTag){
         
-        super(scene, positionX, positionY, spriteTag);
-        scene.add.existing(this);
-        scene.physics.world.enable(this);
+        super(_scene, _positionX, _positionY, _spriteTag);
+        _scene.add.existing(this);
+        _scene.physics.world.enable(this);
         this.body.collideWorldBounds = true;
         this.body.setBounce(1, 1);
-
+        this.scene = _scene;
 
         this.cursors = scene.input.keyboard.createCursorKeys();
         
@@ -32,14 +32,96 @@ class Pad extends Phaser.GameObjects.Sprite
         super.preUpdate(time, delta); 
     }
 
-    /*
-    setColliders()
+    SetPadZones()
     {
-        this.scene.physics.add.collider
+        this.getTopLeft();   // -> go left-up (diagonal) -> affect velocity.x & velocity.y
+        this.getTopCenter(); // -> go up (straight) -> affect only velocity.y
+        this.getTopRight();  // -> go right-up (diagonal) -> affect velocity.x & velocity.y
+
+        distanceLeftToCenter = Phaser.Math.Distance(this.getTopLeft(), this.getTopCenter);
+        distanceCenterToRight = Phaser.Math.Distance(this.getTopCenter(), this.getTopRight());
+
+        leftBound.x = this.getTopLeft().x + distanceLeftToCenter/2;
+        leftBound.y = this.getTopLeft().y;
+        
+        centerBound_left.x = this.getTopCenter().x - distanceLeftToCenter/2;
+        centerBound_left.y = this.getTopCenter().y; 
+        centerBound_right.x = this.getTopCenter().x + distanceCenterToRight/2;
+        centerBound_right.y = this.getTopCenter().y;
+
+        rightBound.x = this.getTopRight().x - distanceCenterToRight/2;
+        rightBound.y = this.getTopRight().y;
+
+        this.leftZone = [
+            this.getTopLeft(),
+            leftBound
+        ];
+
+        this.centerZone = [
+            centerBound_left,
+            centerBound_right
+        ];
+
+        this.rightZone = [
+            rightBound,
+            this.getTopRight()
+        ];
+    }
+    
+    SetColliders()
+    {
+        /*
+        this.scene.physics.add.overlap
         (
             this,
-            this.scene.ball
-        )
+            this.scene.ball,
+            this.ApplyBounce,
+            null,
+            this
+        );
+        */
+
+        this.physics.add.overlap(
+            this,
+            this.scene.ball,
+            function overlap(pad, ball) {
+              //ball.setFrame(5).disableBody();
+              var { x, y } = ball.body.center;                   
+              ballLocalPoint = ball.getLocalPoint(x, y, localPoint);
+
+                //Compare ballLocalPoint to pad thresholds
+                //Apply bounce with multiplier according to comparison
+
+                if(ballLocalPoint.x >= leftZone[0] && ballLocalPoint < leftZone[1])
+                {
+                    ball.ApplyBounce('diagonal to up left')
+                }
+
+                else if(ballLocalPoint.x >= centerZone[0] && ballLocalPoint < centerZone[1])
+                {
+                    ball.ApplyBounce('up')
+                }
+
+                else if(ballLocalPoint.x >= rightZone[0] && ballLocalPoint < rightZone[1])
+                {
+                    ball.ApplyBounce('diagonal to up right')
+                }
+
+            },
+            null,
+            this
+          );
+
     }
-    */
+
+    ApplyBounce(_ball)
+    {
+        ball.ChangeVelocity(2);
+    }
+
+    CheckInput()
+    {
+
+    }
+    
 }
