7901109ea17805ac64efc2f2d9c5b501d95f629a
adri2711 <adri271101@gmail.com>
2023-11-10 18:59:44 +0100

    lives display

--- ArkanoidReturns/js/Prefabs/blockPrefab.js
+++ ArkanoidReturns/js/Prefabs/blockPrefab.js
@@ -1,24 +1,37 @@
-class blockPrefab extends Phaser.GameObjects.Sprite
+class BlockPrefab extends Phaser.GameObjects.Sprite
 {
-    constructor(_scene,_posX,_posY,_spriteTag, _animTag, health)
+    constructor(_scene,_posX,_posY,_spriteTag, _animTag, _health, _ball)
     {
-        //this.nave = Scene.physics.add.sprite(posX,posY,spriteTag);
         super(_scene,_posX,_posY,_spriteTag);
         _scene.add.existing(this);
-
+        
         _scene.physics.world.enable(this);
         this.body.setImmovable(true);
-        this.health = health;
+        this.collider = _scene.physics.add.collider(this, _ball);
+        this.health = _health;
         if (_animTag != null) {
             this.anims.play(_animTag);
         }
+        
+        this.body.onCollide = true;
+        _scene.physics.world.on('collide', (gameObject1, gameObject2, body1, body2) => {
+            gameObject1.damage();
+        })
+        this.damaged = false;
     }
 
-    damage(_block, _damageAgent) {
+    damage() {
+        if (this.damaged) return;
+        this.damaged = true;
         this.health--;
-        if (this.health < 0) {
+        if (this.health <= 0) {
             this.deActivate();
         }
+    }
+
+    preUpdate(time, delta) {
+        super.preUpdate(time, delta);
+        this.damaged = false;
     }
 
     deActivate() {
--- ArkanoidReturns/js/Scenes/TestLevel.js
+++ ArkanoidReturns/js/Scenes/TestLevel.js
@@ -7,6 +7,7 @@ class TestLevel extends Phaser.Scene
 
     preload()
     {
+        this.load.spritesheet("pad", "assets/img/pad/default.png", {frameWidth: 88, frameHeight: 22});
         this.load.setPath("assets/img/projectile");
         this.load.image("normalBall", "cyan.png");
         this.load.setPath("assets/img/block");
@@ -18,10 +19,23 @@ class TestLevel extends Phaser.Scene
         this.loadAnimations();
 
         this.cameras.main.setBackgroundColor("003");
-        this.ball = new NormalBall(this, config.width / 2, config.height / 2);
+        this.ball = new NormalBall(this, config.width / 2, 2* config.height / 3);
 
-        this.block1 = new blockPrefab(this, config.width/2, config.height/3, 'silverBlock', 'silverBlockAnim', 1).setScale(0.5);
-        this.block2 = new blockPrefab(this, config.width/2 + 22, config.height/3, 'silverBlock', null, 1).setScale(0.5);
+        this.block1 = new BlockPrefab(this, config.width/2, config.height/4, 'silverBlock', 'silverBlockAnim', 2, this.ball).setScale(0.5);
+        this.block2 = new BlockPrefab(this, config.width/2 + 22, config.height/4, 'silverBlock', null, 1, this.ball).setScale(0.5);
+
+        this.livesIcon = this.add.sprite(22, config.width / 2 - 35, 'pad', 0).setScale(.3);
+        this.livesDisplay = this.add.text(
+            this.livesIcon.width * this.livesIcon.scale + this.livesIcon.x - 5,
+            config.height / 2,
+            "x 4",
+            {
+                fontFamily: 'Arial',
+                fill: '#FFFFFF',
+                fontSize: 12
+            }
+        )
+        this.livesDisplay.setTint(0x40ff80, 0x40ff80, 0xffb000, 0xffb000);
 
         /*this.time.addEvent
         (
@@ -37,11 +51,17 @@ class TestLevel extends Phaser.Scene
 
     loadAnimations()
     {
-        
         this.anims.create(
         {
             key: 'silverBlockAnim',
             frames:this.anims.generateFrameNumbers('silverBlock', {start:0, end: 9}),
+            frameRate: 15,
+            repeat: -1
+        });
+        this.anims.create(
+        {
+            key: 'padAnim',
+            frames:this.anims.generateFrameNumbers('pad', {start:0, end: 4}),
             frameRate: 15,
             repeat: -1
         });
