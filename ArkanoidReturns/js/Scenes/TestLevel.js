97788a37298420f2e2cbfcd1f86ac01697ef85b0
Iris LC <iris.lazaro@enti.cat>
2023-11-10 19:25:44 +0100

    Revert "Merge branch 'feature/pad' into develop"
    This reverts commit 2a921498937087ad216272be73cc7b5de3cad27b, reversing
    changes made to 7901109ea17805ac64efc2f2d9c5b501d95f629a.

--- ArkanoidReturns/js/Scenes/TestLevel.js
+++ ArkanoidReturns/js/Scenes/TestLevel.js
@@ -7,13 +7,11 @@ class TestLevel extends Phaser.Scene
 
     preload()
     {
+        this.load.spritesheet("pad", "assets/img/pad/default.png", {frameWidth: 88, frameHeight: 22});
         this.load.setPath("assets/img/projectile");
         this.load.image("normalBall", "cyan.png");
         this.load.setPath("assets/img/block");
         this.load.spritesheet("silverBlock", "silver_animated.png", {frameWidth: 44, frameHeight: 22});
-        
-        this.load.setPath("assets/img/pad")
-        this.load.spritesheet("pad", "default.png", {frameWidth: 88, frameHeight:22});
     }
 
     create()
@@ -21,11 +19,23 @@ class TestLevel extends Phaser.Scene
         this.loadAnimations();
 
         this.cameras.main.setBackgroundColor("003");
-        this.ball = new NormalBall(this, config.width / 2, config.height / 2);
-        this.pad = new Pad(this, config.width/2, config.height, 'pad', 'padAnim').setScale(0.5);
+        this.ball = new NormalBall(this, config.width / 2, 2* config.height / 3);
 
-        this.block1 = new blockPrefab(this, config.width/2, config.height/3, 'silverBlock', 'silverBlockAnim', 1).setScale(0.5);
-        this.block2 = new blockPrefab(this, config.width/2 + 22, config.height/3, 'silverBlock', null, 1).setScale(0.5);
+        this.block1 = new BlockPrefab(this, config.width/2, config.height/4, 'silverBlock', 'silverBlockAnim', 2, this.ball).setScale(0.5);
+        this.block2 = new BlockPrefab(this, config.width/2 + 22, config.height/4, 'silverBlock', null, 1, this.ball).setScale(0.5);
+
+        this.livesIcon = this.add.sprite(22, config.width / 2 - 35, 'pad', 0).setScale(.3);
+        this.livesDisplay = this.add.text(
+            this.livesIcon.width * this.livesIcon.scale + this.livesIcon.x - 5,
+            config.height / 2,
+            "x 4",
+            {
+                fontFamily: 'Arial',
+                fill: '#FFFFFF',
+                fontSize: 12
+            }
+        )
+        this.livesDisplay.setTint(0x40ff80, 0x40ff80, 0xffb000, 0xffb000);
 
         /*this.time.addEvent
         (
@@ -41,7 +51,6 @@ class TestLevel extends Phaser.Scene
 
     loadAnimations()
     {
-        
         this.anims.create(
         {
             key: 'silverBlockAnim',
@@ -49,11 +58,10 @@ class TestLevel extends Phaser.Scene
             frameRate: 15,
             repeat: -1
         });
-
         this.anims.create(
         {
             key: 'padAnim',
-            frames:this.anims.generateFrameNumbers('pad', {start:0, end: 3}),
+            frames:this.anims.generateFrameNumbers('pad', {start:0, end: 4}),
             frameRate: 15,
             repeat: -1
         });
