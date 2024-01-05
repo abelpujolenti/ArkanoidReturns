504c921c9e83ba2b726b84d0eda15d70d56e871c
Iris LC <iris.lazaro@enti.cat>
2024-01-05 19:25:01 +0100

    Spawn random power up

--- ArkanoidReturns/js/Prefabs/CrystalBlockPrefab.js
+++ ArkanoidReturns/js/Prefabs/CrystalBlockPrefab.js
@@ -3,12 +3,11 @@ class CrystalBlockPrefab extends BlockPrefab
     constructor(_scene,_posX,_posY,_spriteTag, _animTag, _health, _ball, _pad, _score, ballHitBrickSound)
     {
         super(_scene,_posX,_posY,_spriteTag, _animTag, _health, _ball, _pad, _score, ballHitBrickSound);
-        this.powerUpTypes = ["E", "L", "B", "G", "D", "M", "T", "P", "S", "C"];
+        this.powerUpTypes = ["E", "L", "B", "D", "P", "S", "C"];
     }
 
     break() {
-        //this.scene.SpawnPowerup(this, this.powerUpTypes[this.GetRandomPowerUpType()]);
-        this.scene.SpawnPowerup(this, "B");
+        this.scene.SpawnPowerup(this, this.powerUpTypes[this.GetRandomPowerUpType()]);
         super.break();
     }
 
